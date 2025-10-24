d7a55d01e204b4970f2c76da379cfc94eab39b88
yasminem <yasminemoukhliss@aol.com>
2025-10-24 16:08:05 +0200

    Add files via upload

--- raclette.py
+++ raclette.py
@@ -22,3 +22,14 @@ def Get_events():
 def add_events(event, tab_event):
 	tab_event.append(event)
 	return tab_event
+
+def first_event(tab_event):
+	print(tab_event[0])
+	return tab_event[0]
+
+
+#A finir
+def sort_events(tab_event):
+	tab_event_sorted = []
+	for i in tab_event:
+		if event[0]
--- test2.py
+++ test2.py
@@ -4,12 +4,18 @@ begin_date = "20/01/2020"
 time_event = 30
 name = "Session Raclette"
 event = {}
+tab_event = [event]
 
 def test_createEvents():
-	assert raclette.createEvents(begin_date, time_event ,name) == (1579478400.0, 30, "Session Raclette")
+	assert raclette.createEvents(begin_date, time_event ,name) == (1579474800.0, 30, "Session Raclette")
 		
 def test_Get_events():
 	assert raclette.Get_events() == []
 	
 def test_add(): 
 	assert raclette.add_events(event,[]) == [{}]
+
+def test_first_event(tab_event):
+	assert raclette.first_event([]) = tab_event[0]
+
+#A finir
