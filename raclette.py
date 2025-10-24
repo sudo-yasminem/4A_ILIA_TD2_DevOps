cb3f3f83bb8977244da61ee165a660610ecdeeb6
yasminem <yasminemoukhliss@aol.com>
2025-10-24 15:55:39 +0200

    Add files via upload

---
+++ raclette.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+tab_event = []
+begin_date = "20/01/2020"
+time_event = 30
+name = "Session Raclette"
+
+def createEvents(begin_date, time_event, name):
+	date = datetime.strptime (begin_date, "%d/%m/%Y")
+	res_date = date.timestamp()
+	
+	event = (res_date, time_event, name)
+	print(event)
+	return event
+	
+
+def Get_events():
+	return tab_event
+	print(tab_event)
+
+
+def add_events(event, tab_event):
+	tab_event.append(event)
+	return tab_event
--- test.py
+++ test.py
@@ -1,13 +1,15 @@
-import calendrier
+import raclette
 
-"""def test_events():
-	assert Get_events() == []
+begin_date = "20/01/2020"
+time_event = 30
+name = "Session Raclette"
+event = {}
 
-
-def test_creation() :
-	assert createEvents(begin_date, time_event, name) == {begin_date, time_event, name}
-"""
-
+def test_createEvents():
+	assert raclette.createEvents(begin_date, time_event ,name) == 							(1579474800.0, 30, "Session Raclette")
+		
+def test_Get_events():
+	assert raclette.Get_events() == []
+	
 def test_add(): 
-	assert Add_Events(event,tab_event) == [{event}]
-
+	assert raclette.add_events(event,[]) == [{}]
